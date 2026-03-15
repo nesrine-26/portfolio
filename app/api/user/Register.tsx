b5ee88d6c125815a8073c8003152a0686ac8f682
Nessrine88 <macherki.nessrine@yahoo.com>
2026-03-15 16:15:20 +0100

    comment out the register file to use it in the future

--- app/api/user/Register.tsx
+++ app/api/user/Register.tsx
@@ -1,14 +1,14 @@
-import { RegisterUserDto } from "@/dtos";
-import { NextResponse, NextRequest } from "next/server";
+// import { RegisterUserDto } from "@/dtos";
+// import { NextResponse, NextRequest } from "next/server";
 
-export async function Post(request: NextRequest) {
-    try {
-        const body = await request.json() as RegisterUserDto
-        const validation = registerSchema.safeParse(body)
-        if (!validation.success) {
-            return NextResponse.json({error: "Invalid data"}, {status: 400})
-        }
-    } catch (error) {
-       return NextResponse.json({error: "An error occurred"}, {status: 500}) 
-    }
-}
+// export async function Post(request: NextRequest) {
+//     try {
+//         const body = await request.json() as RegisterUserDto
+//         const validation = registerSchema.safeParse(body)
+//         if (!validation.success) {
+//             return NextResponse.json({error: "Invalid data"}, {status: 400})
+//         }
+//     } catch (error) {
+//        return NextResponse.json({error: "An error occurred"}, {status: 500}) 
+//     }
+// }
